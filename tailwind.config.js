bce5067121c6d8bead1670282023c69757cbc928
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-09-13 11:26:21 +0500

    feat: configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      "lightergray": "#959494",
+      "white": "#ffffff",
+      "darkergray": "#454545",
+      "headingColor": "#333232",
+      "scarlet": "#FF794F",
+      "footerColor": "#AFAFAF",
+    },
     extend: {},
   },
   plugins: [],
