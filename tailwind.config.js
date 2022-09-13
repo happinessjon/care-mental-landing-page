7e966fe0bc2a0394cd831ebbfa366b49e5f1eb7b
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-09-13 21:04:57 +0500

    feat: add another section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      mobile: "460px",
+      tablet: "840px",
+      laptop: "1024px",
+      desktop: "1260px",
+      "desktop-xl": "1920px",
+    },
     colors: {
       "lightergray": "#959494",
       "white": "#ffffff",
       "darkergray": "#454545",
       "headingColor": "#333232",
-      "scarlet": "#FF794F",
+      "scarlet": "rgba(255, 121, 79, 0.05)",
       "footerColor": "#AFAFAF",
     },
     extend: {},
